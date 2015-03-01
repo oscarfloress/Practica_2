881852be210529947c933d7a49934f852216e8f7
oscarfloress <oscar.flores@alumno.udg.mx>
2015-02-28 20:31:27 -0600

    Estetica de la salida mas formato de doce horas

--- Practica_2_Reloj/Practica_2_Reloj/Program.cs
+++ Practica_2_Reloj/Practica_2_Reloj/Program.cs
@@ -20,6 +20,7 @@ namespace Practica_2_Reloj
                                 for (int segundo = 0; segundo < 60; segundo++)
                                 {
                                     Console.Clear();
+                                    Console.Write("\n\t+--------------+\n\t|   ");
                                     if (hora < 10)
                                         Console.Write("0" + hora + ":");
                                     else Console.Write(hora + ":");
@@ -29,6 +30,7 @@ namespace Practica_2_Reloj
                                     if (segundo < 10)
                                         Console.Write("0" + segundo);
                                     else Console.Write(segundo);
+                                    Console.Write("   |\n\t+--------------+");
                                     Thread.Sleep(1000);
                                     if (Console.KeyAvailable)
                                     {
@@ -45,15 +47,25 @@ namespace Practica_2_Reloj
                     {
                         Console.Clear();
                         fechaHoy = DateTime.Now;
-                        if (fechaHoy.Hour < 10)
-                            Console.Write("0" + fechaHoy.Hour + ":");
-                        else Console.Write(fechaHoy.Hour + ":");
+                        int horaActual = fechaHoy.Hour;
+                        string formato = "";
+                        Console.Write(" \n     +-------------------+\n     |   ");
+                        if (horaActual > 12)
+                        {
+                            horaActual -= 12;
+                            formato = " p.m.";
+                        }
+                        else formato = " a.m.";
+                        if (horaActual < 10)
+                            Console.Write("0" + horaActual + ":");
+                        else Console.Write(horaActual + ":");
                         if (fechaHoy.Minute < 10)
                             Console.Write("0" + fechaHoy.Minute + ":");
                         else Console.Write(fechaHoy.Minute + ":");
                         if (fechaHoy.Second < 10)
-                            Console.Write("0" + fechaHoy.Second);
-                        else Console.Write(fechaHoy.Second);
+                            Console.Write("0" + fechaHoy.Second + formato);
+                        else Console.Write(fechaHoy.Second + formato);
+                        Console.Write("   |\n     +-------------------+");
                         Thread.Sleep(1000);
                         if (Console.KeyAvailable)
                         {
